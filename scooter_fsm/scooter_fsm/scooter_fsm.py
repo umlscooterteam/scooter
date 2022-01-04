bae5177c12b3330239421a463920605905c43904
Sasha <sashaiw@gmail.com>
2022-01-03 20:07:56 -0500

    added send_request function

--- scooter_fsm/scooter_fsm/scooter_fsm.py
+++ scooter_fsm/scooter_fsm/scooter_fsm.py
@@ -3,19 +3,56 @@ from rclpy.node import Node
 
 from scooter_fsm.states import *
 
+# TODO: actually make these interfaces
+# # scooter services
+# from scooter_interfaces.srv import GetCloud, Segmentation, Pick
+# # UI services
+# from scooter_interfaces.srv import WaitForBegin, PickSelection, PickSelectionConfirm, HoldingObject, Basket
+
 
 class ScooterFSMNode(Node):
     """Node that handles state transitions"""
     def __init__(self):
         super().__init__('scooter_fsm')
-        self.test_string = "h"
 
-        state = DriveMode()
+        self.clients = {
+            GetCloud: self.create_client(GetCloud, "get_cloud"),
+            Segmentation: self.create_client(Segmentation, "segmentation"),
+            Pick: self.create_client(Pick, "pick"),
+            WaitForBegin: self.create_client(WaitForBegin, "wait_for_begin"),
+            PickSelection: self.create_client(PickSelection, "pick_selection"),
+            PickSelectionConfirm: self.create_client(PickSelectionConfirm, "pick_selection_confirm"),
+            HoldingObject: self.create_client(HoldingObject, "holding_object"),
+            Basket: self.create_client(Basket, "basket")
+        }
 
         # loop through states defined in scooter_fsm.states
+        state = DriveMode()
         while rclpy.ok():
             state = state.run(self)
             self.get_logger().info(f"Now in state: {state}")
+
+    def send_request(self, request):
+        """
+        :param request: The message to send a request for
+        :return: The result from the service
+        """
+        try:
+            # get client from type of message
+            client = self.client[type(request)]
+
+            # wait for the client to come up
+            while not client.wait_for_service(timeout_sec=1.0):
+                self.get_logger().info("Service not available, trying again in 1s...")
+
+            # call service, return result
+            future = self.cli.call_async(request)
+            self.spin_until_future_complete(future)
+            return future.result()
+
+        except KeyError as e:
+            # if we do not have a client defined for this service, exit
+            self.get_logger().error(f"send_request called for invalid service type: {e}")
 
 
 def main(args=None):
--- scooter_fsm/scooter_fsm/states.py
+++ scooter_fsm/scooter_fsm/states.py
@@ -1,5 +1,10 @@
 from scooter_fsm.state import State
 
+# scooter services
+# from scooter_interfaces.srv import GetCloud, Segmentation, Pick
+# UI services
+# from scooter_interfaces.srv import WaitForBegin, PickSelection, PickSelectionConfirm, HoldingObject, Basket
+
 
 class DriveMode(State):
     """Waiting for user to begin pick action
@@ -8,5 +13,5 @@ class DriveMode(State):
     """
     @staticmethod
     def run(node):
-        print(node.test_string)
+        node.send_request(wait_for_begin_request)
         return DriveMode()
