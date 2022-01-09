48a795326680c7cd5cc972b71f477c8f654c5720
Sam Laderoute <laderoute.samuel@gmail.com>
2022-01-08 20:19:15 -0500

    scooter_core launch file now launches necessary action servers

--- scooter_core/launch/scooter_core.launch.py
+++ scooter_core/launch/scooter_core.launch.py
@@ -3,11 +3,22 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    get_cloud_action_server = Node(
+        package='scooter_perception',
+        executable='get_cloud_action_server',
+    )
+    segmentation_action_server = Node(
+        package='scooter_perception',
+        executable='segmentation_action_server',
+    )
+    scooter_fsm = Node(
+        package="scooter_fsm",
+        executable="scooter_fsm",
+    )
+
     return LaunchDescription([
-        Node(
-            package="scooter_fsm",
-            namespace="scooter_fsm",
-            executable="scooter_fsm",
-            name="scooter_fsm_node"
-        )
+        get_cloud_action_server,
+        segmentation_action_server,
+        scooter_fsm,
     ])
