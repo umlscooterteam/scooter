8f01c46e2a56d3ea3075b902dc0e00e160142d3e
Sasha <sashaiw@gmail.com>
2022-01-19 18:06:57 -0500

    reformatted to Google C++ style guide

--- scooter_manipulation/src/static_collision_object_publisher.cpp
+++ scooter_manipulation/src/static_collision_object_publisher.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 #include "rclcpp/rclcpp.hpp"
 
-
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
@@ -23,66 +22,63 @@ using namespace std;
 
 const std::string WHITESPACE = " \n\r\t\f\v";
 
-string ltrim(const std::string &s)
-{
-    size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == string::npos) ? "" : s.substr(start);
+string ltrim(const std::string &s) {
+  size_t start = s.find_first_not_of(WHITESPACE);
+  return (start == string::npos) ? "" : s.substr(start);
 }
 
-string rtrim(const std::string &s)
-{
-    size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == string::npos) ? "" : s.substr(0, end + 1);
+string rtrim(const std::string &s) {
+  size_t end = s.find_last_not_of(WHITESPACE);
+  return (end == string::npos) ? "" : s.substr(0, end + 1);
 }
 
 string trim(const string &s) {
-    return rtrim(ltrim(s));
+  return rtrim(ltrim(s));
 }
 
-vector<vector<string>> read_csv(string f_name)
-{
-    // File pointer
-    ifstream fin (f_name);
-    // Open an existing file
-    //fin.open(f_name, ios::in);
-    if (!fin.is_open()) {
-        cout << "Error opening file: " << f_name << endl;
-        exit (1);
+vector<vector<string>> read_csv(string f_name) {
+  // File pointer
+  ifstream fin(f_name);
+  // Open an existing file
+  //fin.open(f_name, ios::in);
+  if (!fin.is_open()) {
+    cout << "Error opening file: " << f_name << endl;
+    exit(1);
+  }
+
+  // Read the Data from the file
+  // as String Vector
+  vector<vector<string>> data;
+  vector<string> row;
+  string line, word, temp;
+
+  while (!fin.eof()) {
+    row.clear();
+
+    // read an entire row and
+    // store it in a string variable 'line'
+    getline(fin, line);
+
+    // used for breaking words
+    stringstream s(line);
+
+    // read every column data of a row and
+    // store it in a string variable, 'word'
+    while (getline(s, word, ',')) {
+
+      // add all the column data
+      // of a row to a vector
+      string w = trim(word);  // trim off white space
+      w.erase(remove(w.begin(), w.end(), '\"'), w.end());  // get rid of double quotes
+      row.push_back(w);
     }
-
-    // Read the Data from the file
-    // as String Vector
-    vector<vector<string>> data;
-    vector<string> row;
-    string line, word, temp;
-
-    while (! fin.eof()) {
-        row.clear();
-
-        // read an entire row and
-        // store it in a string variable 'line'
-        getline(fin, line);
-
-        // used for breaking words
-        stringstream s(line);
-
-        // read every column data of a row and
-        // store it in a string variable, 'word'
-        while (getline(s, word, ',')) {
-
-            // add all the column data
-            // of a row to a vector
-            string w = trim(word);  // trim off white space
-            w.erase(remove(w.begin(), w.end(), '\"'), w.end());  // get rid of double quotes
-            row.push_back(w);
-        }
-          data.push_back(row);
-    }
-    return data;
+    data.push_back(row);
+  }
+  return data;
 }
 
-void add_box_to_collision_environment(moveit::planning_interface::MoveGroupInterface* move_group,
-                                      moveit::planning_interface::PlanningSceneInterface* planning_scene_interface,
+void add_box_to_collision_environment(moveit::planning_interface::MoveGroupInterface *move_group,
+                                      moveit::planning_interface::PlanningSceneInterface *planning_scene_interface,
                                       vector<string> collision_object_data) {
   // collision object data:
   // name   pos_x   pos_y   pos_z   euler_x   euler_y   euler_z   dim_x   dim_y   dim_z
@@ -107,9 +103,9 @@ void add_box_to_collision_environment(moveit::planning_interface::MoveGroupInter
   box_pose.position.x = std::stod(collision_object_data[1]);  // setting position
   box_pose.position.y = std::stod(collision_object_data[2]);
   box_pose.position.z = std::stod(collision_object_data[3]);
-  double r = 3.14159*(std::stod(collision_object_data[4]))/180.0;  // setting orientation (convert to rads)
-  double p = 3.14159*(std::stod(collision_object_data[5]))/180.0;
-  double y = 3.14159*(std::stod(collision_object_data[6]))/180.0;
+  double r = 3.14159 * (std::stod(collision_object_data[4])) / 180.0;  // setting orientation (convert to rads)
+  double p = 3.14159 * (std::stod(collision_object_data[5])) / 180.0;
+  double y = 3.14159 * (std::stod(collision_object_data[6])) / 180.0;
   tf2::Quaternion myQuaternion;
   myQuaternion.setRPY(r, p, y);
   box_pose.orientation.x = myQuaternion[0];
@@ -129,8 +125,7 @@ void add_box_to_collision_environment(moveit::planning_interface::MoveGroupInter
   planning_scene_interface->addCollisionObjects(collision_objects);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   // init ROS stuff
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
@@ -153,11 +148,11 @@ int main(int argc, char** argv)
   vector<vector<string>> data = read_csv(env_csv_file_path);
   vector<string> collision_obj;
   for (unsigned int i = 1; i < data.size(); i++) {  // first row (row 0) is just headers so we can skip those
-      collision_obj.clear();
-      for (unsigned int j = 0; j < data[i].size(); j++) {
-          collision_obj[j] = data[i][j];
-          add_box_to_collision_environment(&move_group, &planning_scene_interface, collision_obj);
-      }
+    collision_obj.clear();
+    for (unsigned int j = 0; j < data[i].size(); j++) {
+      collision_obj[j] = data[i][j];
+      add_box_to_collision_environment(&move_group, &planning_scene_interface, collision_obj);
+    }
   }
 
   rclcpp::shutdown();
