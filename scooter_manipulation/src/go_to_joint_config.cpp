c98adf49d9b4ee0861aef0e11a5c1b977d0436b6
sashaiw <sashaiw@gmail.com>
2022-02-22 16:59:12 -0500

    fixed runtime error -- apparently the node name was too long
    Unfortunately, the future is not allowing the action to block until the move_group_.move() returns, but it is unclear for now whether this is an issue with `go_to_joint_config.cpp` or `test_go_to_joint_config.py` which I am using to test.

--- scooter_manipulation/src/go_to_joint_config.cpp
+++ scooter_manipulation/src/go_to_joint_config.cpp
@@ -16,15 +16,14 @@
 #define PLANNING_GROUP "ur_manipulator"
 
 namespace go_to_joint_config_cpp {
- class GTJCActionServer : public rclcpp::Node, std::enable_shared_from_this<GTJCActionServer> {
+ class GTJCActionServer : public rclcpp::Node {
   public:
    using GTJC = scooter_interfaces::action::GoToJointConfig;
    using GoalHandleGTJC = rclcpp_action::ServerGoalHandle<GTJC>;
 
-   explicit GTJCActionServer(std::string name,
-                             std::shared_ptr<rclcpp::Node> move_group_node,
+   explicit GTJCActionServer(std::shared_ptr<rclcpp::Node> move_group_node,
                              const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
-       : Node(name, options) {
+       : Node("go_to_joint_config", options) {
      using namespace std::placeholders;
 
      move_group_node_ = move_group_node;
@@ -34,10 +33,6 @@ namespace go_to_joint_config_cpp {
      move_group_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(move_group_node_, PLANNING_GROUP);
 
      // action server stuff
-     rclcpp::executors::SingleThreadedExecutor executor;
-     executor.add_node(static_cast<std::shared_ptr<Node>>(this));
-     std::thread([&executor]() {executor.spin();}).detach();
-
      this->action_server_ = rclcpp_action::create_server<GTJC>(
        this,
        "go_to_joint_config",
@@ -47,7 +42,7 @@ namespace go_to_joint_config_cpp {
      );
    }
 
-   ~GTJCActionServer() override = default;
+   // ~GTJCActionServer() override = default;
 
   private:
    // MoveIt stuff
@@ -83,12 +78,19 @@ namespace go_to_joint_config_cpp {
    }
 
    void execute(const std::shared_ptr<GoalHandleGTJC> goal_handle) {
-     const auto goal = goal_handle->get_goal();
      RCLCPP_INFO(this->get_logger(), "Going to joint config");
 
-     // std::vector<double> joint_config(goal->position);
+     const auto goal = goal_handle->get_goal();
+     auto result = std::make_shared<GTJC::Result>();
 
      move_group_->setJointValueTarget(goal->position);
+     move_group_->move();
+
+     if (rclcpp::ok()) {
+         result->success = true;
+         goal_handle->succeed(result);
+         RCLCPP_INFO(this->get_logger(), "Go to joint config succeeded!");
+     }
    }
 
  }; // class GTJCActionServer
@@ -98,11 +100,15 @@ int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
   node_options.automatically_declare_parameters_from_overrides(true);
-  rclcpp::Node::SharedPtr move_group_node = rclcpp::Node::make_shared("moveit_move_group_helper_node", node_options);
+  rclcpp::Node::SharedPtr move_group_node = rclcpp::Node::make_shared("gtjc_move_group_node", node_options);
 
-  rclcpp::spin(std::make_shared<go_to_joint_config_cpp::GTJCActionServer>("go_to_joint_config",
-                                                                                   move_group_node,
-                                                                                   node_options));
+  // spin move_group_node
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_node);
+  std::thread([&executor]() {executor.spin();}).detach();
+
+  // spin action server node
+  rclcpp::spin(std::make_shared<go_to_joint_config_cpp::GTJCActionServer>(move_group_node, node_options));
   rclcpp::shutdown();
   return 0;
 }
